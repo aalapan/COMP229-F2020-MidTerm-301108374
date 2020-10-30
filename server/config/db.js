99c6088372c727226c132d24af1f84aa20af2aa4
aalapan <aalapan@my.centennialcollege.ca>
2020-10-30 18:10:42 -0400

    Edited Tables and Added Buttons

--- server/config/db.js
+++ server/config/db.js
@@ -1,4 +1,4 @@
 module.exports = {
   //local MongoDB deployment ->
-  "URI": "mongodb://localhost/books229"
+  "URI": "mongodb+srv://AAlapan:BD859lOAE42F7Dcd@mongodbserver.oxaws.mongodb.net/book_store?retryWrites=true&w=majority"
 };
